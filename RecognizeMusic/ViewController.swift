0b863ac229ff59586e994cd0b72e444a68f497ea
Daniel <dnl.canedo@gmail.com>
2022-06-23 22:44:07 -0400

    WIP

--- RecognizeMusic/ViewController.swift
+++ RecognizeMusic/ViewController.swift
@@ -11,91 +11,95 @@ import ShazamKit
 class ViewController: UIViewController, SHSessionDelegate {
 
     @IBOutlet weak var songInfoTxt: UILabel!
+    let audioEngine = AVAudioEngine()
+    let mixerNode = AVAudioMixerNode()
+
+    // The session for the active ShazamKit match request.
+    var session: SHSession?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         songInfoTxt.text = "recognizing..."
+
+        Task(priority: .background) {
+            do {
+                guard await AVAudioSession.sharedInstance().hasPermissionToRecord() else {
+                    throw NSError()
+                }
+            } catch {
+//                speakError(error)
+                throw NSError()
+            }
+        }
+
         recognizeSong()
     }
 
     private func recognizeSong() {
-        // Session
-        let session = SHSession()
-        // Delegate
-        session.delegate = self
-
         do {
-            // Get track
-            guard let url = Bundle.main.url(forResource: "song", withExtension: "m4a") else {
-                print("Failed to get song url")
-                return
-            }
-
-            guard let audioFormat = AVAudioFormat(standardFormatWithSampleRate: 44100, channels: 1) else {
-                print("Error at creating audio format")
-                return
-            }
-
-            let generator = SHSignatureGenerator()
-
-            // create audio file
-            let audioFile = try AVAudioFile(forReading: url)
-
-            guard
-                let inputBuffer = AVAudioPCMBuffer(pcmFormat: audioFile.processingFormat, frameCapacity: 44100 * 10),
-                let outputBuffer = AVAudioPCMBuffer(pcmFormat: audioFormat, frameCapacity: 44100 * 10)
-            else {
-                print("error at creating input/output buffer")
-                return
-            }
-
-            let inputBlock: AVAudioConverterInputBlock = { inNumPackets, outStatus in
-                do {
-                    try audioFile.read(into: inputBuffer)
-                    outStatus.pointee = .haveData
-                    return inputBuffer
-                } catch {
-                    if audioFile.framePosition >= audioFile.length {
-                        outStatus.pointee = .endOfStream
-                        return nil
-                    } else {
-                        outStatus.pointee = .noDataNow
-                        return nil
-                    }
-                }
-            }
-
-            guard let converter = AVAudioConverter(from: audioFile.processingFormat, to: audioFormat) else {
-                print("error at converting audio")
-                return
-            }
-
-            let status = converter.convert(to: outputBuffer, error: nil, withInputFrom: inputBlock)
-
-            try generator.append(outputBuffer, at: nil)
-
-            if status == .inputRanDry {
-                return
-            }
-            // Audio -> Buffer
-//            guard let buffer = AVAudioPCMBuffer(
-//                pcmFormat: audioFile.processingFormat,
-//                frameCapacity: AVAudioFrameCount(audioFile.length / 2)
-//            ) else {
-//                print("Failed to create the buffer")
-//                return
-//            }
-//
-//            try audioFile.read(into: buffer)
-//            // Signature generator
-//            let generator = SHSignatureGenerator()
-//            try generator.append(buffer, at: nil)
-            // Create signature
-            let signature = generator.signature()
-
-            session.match(signature)
+            configureAudioEngine()
+            try startListening()
         } catch {
             print(error)
+        }
+        // Session
+//        let session = SHSession()
+//        // Delegate
+//        session.delegate = self
+//
+//        do {
+//            let audioEngine = AVAudioEngine()
+//            let mixerNode = AVAudioMixerNode()
+//        } catch {
+//            print(error)
+//        }
+    }
+
+    func addAudio(buffer: AVAudioPCMBuffer, audioTime: AVAudioTime) {
+        // Add the audio to the current match request.
+        session?.matchStreamingBuffer(buffer, at: audioTime)
+    }
+
+    func stopListening() {
+        // Check if the audio engine is already recording.
+        if audioEngine.isRunning {
+            audioEngine.stop()
+        }
+    }
+
+    func startListening() throws {
+        // Throw an error if the audio engine is already running.
+        guard !audioEngine.isRunning else { return }
+        let audioSession = AVAudioSession.sharedInstance()
+
+        // Ask the user for permission to use the mic if required then start the engine.
+        try audioSession.setCategory(.playAndRecord)
+        audioSession.requestRecordPermission { [weak self] success in
+            guard success, let self = self else { return }
+            try? self.audioEngine.start()
+        }
+    }
+
+    func configureAudioEngine() {
+        // Get the native audio format of the engine's input bus.
+        let inputFormat = audioEngine.inputNode.inputFormat(forBus: 0)
+
+        // Set an output format compatible with ShazamKit.
+        let outputFormat = AVAudioFormat(standardFormatWithSampleRate: 48000, channels: 1)
+
+        // Create a mixer node to convert the input.
+        audioEngine.attach(mixerNode)
+
+        // Attach the mixer to the microphone input and the output of the audio engine.
+        audioEngine.connect(audioEngine.inputNode, to: mixerNode, format: inputFormat)
+        audioEngine.connect(mixerNode, to: audioEngine.outputNode, format: outputFormat)
+
+        // Install a tap on the mixer node to capture the microphone audio.
+        mixerNode.installTap(onBus: 0,
+                             bufferSize: 8192,
+                             format: outputFormat) { buffer, audioTime in
+            // Add captured audio to the buffer used for making a match.
+            self.addAudio(buffer: buffer, audioTime: audioTime)
         }
     }
 
@@ -114,10 +118,31 @@ class ViewController: UIViewController, SHSessionDelegate {
         }
     }
 
+    func match(_ catalog: SHCustomCatalog) throws {
+        // Create a session if one doesn't already exist.
+        if (session == nil) {
+            session = SHSession(catalog: catalog)
+            session?.delegate = self
+        }
+
+        // Start listening to the audio to find a match.
+        try startListening()
+    }
+
     func session(_ session: SHSession, didNotFindMatchFor signature: SHSignature, error: Error?) {
         if let error = error {
             print(error)
             songInfoTxt.text = error.localizedDescription
+        }
+    }
+}
+
+extension AVAudioSession {
+    func hasPermissionToRecord() async -> Bool {
+        await withCheckedContinuation { continuation in
+            requestRecordPermission { authorized in
+                continuation.resume(returning: authorized)
+            }
         }
     }
 }
