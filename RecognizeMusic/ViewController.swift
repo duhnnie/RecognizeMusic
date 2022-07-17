090c9284d40ff597ddff79104563fcf67c78cae4
Daniel <dnl.canedo@gmail.com>
2022-07-17 19:00:54 -0400

    Record audio

--- RecognizeMusic/ViewController.swift
+++ RecognizeMusic/ViewController.swift
@@ -8,131 +8,104 @@
 import UIKit
 import ShazamKit
 
-class ViewController: UIViewController, SHSessionDelegate {
+class ViewController: UIViewController, SHSessionDelegate, AVAudioRecorderDelegate {
 
-    @IBOutlet weak var songInfoTxt: UILabel!
-    let audioEngine = AVAudioEngine()
-    let mixerNode = AVAudioMixerNode()
+//    @IBOutlet weak var songInfoTxt: UILabel!
+    var recordButton: UIButton!
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
 
     // The session for the active ShazamKit match request.
     var session: SHSession?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        songInfoTxt.text = "recognizing..."
+//        songInfoTxt.text = "recognizing..."
 
-        Task(priority: .background) {
-            do {
-                guard await AVAudioSession.sharedInstance().hasPermissionToRecord() else {
-                    throw NSError()
-                }
-            } catch {
-//                speakError(error)
-                throw NSError()
-            }
-        }
+        recordingSession = AVAudioSession.sharedInstance()
 
-        recognizeSong()
-    }
-
-    private func recognizeSong() {
         do {
-            configureAudioEngine()
-            try startListening()
-        } catch {
-            print(error)
-        }
-        // Session
-//        let session = SHSession()
-//        // Delegate
-//        session.delegate = self
-//
-//        do {
-//            let audioEngine = AVAudioEngine()
-//            let mixerNode = AVAudioMixerNode()
-//        } catch {
-//            print(error)
-//        }
-    }
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            try recordingSession.setActive(true)
 
-    func addAudio(buffer: AVAudioPCMBuffer, audioTime: AVAudioTime) {
-        // Add the audio to the current match request.
-        session?.matchStreamingBuffer(buffer, at: audioTime)
-    }
-
-    func stopListening() {
-        // Check if the audio engine is already recording.
-        if audioEngine.isRunning {
-            audioEngine.stop()
-        }
-    }
-
-    func startListening() throws {
-        // Throw an error if the audio engine is already running.
-        guard !audioEngine.isRunning else { return }
-        let audioSession = AVAudioSession.sharedInstance()
-
-        // Ask the user for permission to use the mic if required then start the engine.
-        try audioSession.setCategory(.playAndRecord)
-        audioSession.requestRecordPermission { [weak self] success in
-            guard success, let self = self else { return }
-            try? self.audioEngine.start()
-        }
-    }
-
-    func configureAudioEngine() {
-        // Get the native audio format of the engine's input bus.
-        let inputFormat = audioEngine.inputNode.inputFormat(forBus: 0)
-
-        // Set an output format compatible with ShazamKit.
-        let outputFormat = AVAudioFormat(standardFormatWithSampleRate: 48000, channels: 1)
-
-        // Create a mixer node to convert the input.
-        audioEngine.attach(mixerNode)
-
-        // Attach the mixer to the microphone input and the output of the audio engine.
-        audioEngine.connect(audioEngine.inputNode, to: mixerNode, format: inputFormat)
-        audioEngine.connect(mixerNode, to: audioEngine.outputNode, format: outputFormat)
-
-        // Install a tap on the mixer node to capture the microphone audio.
-        mixerNode.installTap(onBus: 0,
-                             bufferSize: 8192,
-                             format: outputFormat) { buffer, audioTime in
-            // Add captured audio to the buffer used for making a match.
-            self.addAudio(buffer: buffer, audioTime: audioTime)
-        }
-    }
-
-    func session(_ session: SHSession, didFind match: SHMatch) {
-        let matchedMediaItems = match.mediaItems
-
-        matchedMediaItems.forEach { matchedMedia in
-            print(matchedMedia.artist ?? "no artist")
-            print(matchedMedia.title)
-            print(matchedMedia.artworkURL)
-            print(matchedMedia.songs)
-
-            DispatchQueue.main.async {
-                self.songInfoTxt.text = "\(matchedMedia.artist!) - \"\(matchedMedia.title!)\""
+            recordingSession.requestRecordPermission { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        listRecordings()
+//                        self.loadRecordingUI()
+//                        startRecording()
+//                        Timer.scheduledTimer(withTimeInterval: 10.0, repeats: false) { Timer in
+//                            finishRecording(success: true)
+//                        }
+                    } else {
+                        print("There's no permission for recording.")
+                    }
+                }
             }
+        } catch {
+            print("Error at trying to init recording \(error.localizedDescription)")
         }
     }
 
-    func match(_ catalog: SHCustomCatalog) throws {
-        // Create a session if one doesn't already exist.
-        if (session == nil) {
-            session = SHSession(catalog: catalog)
-            session?.delegate = self
-        }
+    func listRecordings() {
+        do {
+            let recordingsDirectory = getDocumentsDirectory()
+            let recordings = try FileManager.default.contentsOfDirectory(atPath: recordingsDirectory.path)
 
-        // Start listening to the audio to find a match.
-        try startListening()
+            print(recordings)
+        } catch {
+            print("An error ocurred at trying to list recordings \(error.localizedDescription)")
+        }
     }
 
-    func session(_ session: SHSession, didNotFindMatchFor signature: SHSignature, error: Error?) {
-        if let error = error {
-            print(error)
-            songInfoTxt.text = error.localizedDescription
+    func loadRecordingUI() {
+        recordButton = UIButton(frame: CGRect(x: 64, y: 64, width: 128, height: 64))
+        recordButton.setTitle("Tap to record", for: .normal)
+        recordButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .title1)
+        recordButton.addTarget(self, action: #selector(recordTapped), for: .touchUpInside)
+        view.addSubview(recordButton)
+    }
+
+    @objc func recordTapped() {
+        print("carajo")
+    }
+
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+
+        return paths[0]
+    }
+
+    func finishRecording(success: Bool) {
+        audioRecorder.stop()
+        audioRecorder = nil
+
+        // Additional threatment depending on success or not
+    }
+
+    func startRecording() {
+        let audioFilename = getDocumentsDirectory().appendingPathComponent("record.m4a")
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+
+//            recordButton.setTitle("")
+        } catch {
+            finishRecording(success: false)
+        }
+    }
+
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishRecording(success: false)
         }
     }
 }
